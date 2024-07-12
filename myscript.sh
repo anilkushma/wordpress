fdb6644b1fa6a7847f0eec45b89b028a2a204ae1
anilkushma <156169682+anilkushma@users.noreply.github.com>
2024-07-12 15:14:38 +0545

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -6,8 +6,8 @@ echo"Addding a worker node to the cluster"
 k3d node create anilworker --cluster anil
  
 echo "Deploying Wordpress to the cluster"
-kubectl apply -f /home/anil/k8s/wordpress/mysql.yaml
-kubectl apply -f /home/anil/k8s/wordpress/wordpress-deployment.yaml
+kubectl apply -f mysql.yaml
+kubectl apply -f wordpress-deployment.yaml
 echo "Verify the deployment"
 kubectl get deployments
 kubectl get pods
